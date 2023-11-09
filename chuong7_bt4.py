f6e884c23a4e097d79990fe8a575bb505cb46dd9
TheAnh205 <148074718+TheAnh205@users.noreply.github.com>
2023-11-09 16:10:16 +0700

    nop bt

---
+++ chuong7_bt4.py
@@ -0,0 +1,18 @@
+x = 10
+y = 4
+print('x = %d, y = %d'%(x,y))
+equivelence = x==y
+print('x==y is', equivelence)
+x==y is False
+equivelence = x!=y
+print('x!=y is', equivelence)
+x!=y is True
+equivelence = x>y
+print('x>y is', equivelence)
+x>y is True
+equivelence = x<y
+print('x<y is', equivelence)
+x<y is False
+equivelence = x<=y
+print('x<=y is', equivelence)
+x<=y is False
